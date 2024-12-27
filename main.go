d466987c07ae4d3a2eb97a0f4df6025631d7a879
Chris-Coleongco <christien.coleongco@gmail.com>
2024-12-26 23:17:51 -0700

    made it look over several dirs i may put in config file

--- main.go
+++ main.go
@@ -5,20 +5,38 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-func open_workflow(workflow string) {
+type choice struct {
+	parent_dir string
+	child_dir string
+}
+
+type model struct {
+	choices  []choice
+	cursor   int
+	selected map[int]struct{}
+}
+
+func open_workflow(workflow choice) {
+
+	home_dir, err := os.UserHomeDir()
+
+	if err != nil {
+		fmt.Println("err occurred getting home_dir", home_dir)
+	}	
+
 	cmd := exec.Command("kitty", "--detach", "tmux")
 
-	cmd.Dir = workflow
+	cmd.Dir = fmt.Sprintf("%s/%s/%s", home_dir, workflow.parent_dir, workflow.child_dir)
 
-	err := cmd.Start()
+	err = cmd.Start()
 	if err != nil {
 		fmt.Println("err with starting workflow", err)
 	}
 	err = cmd.Wait()
-
 	if err != nil {
 		fmt.Println("err with workflow", err)
 	}
@@ -26,36 +44,94 @@ func open_workflow(workflow string) {
 	os.Exit(0)
 }
 
-type model struct {
-	choices  []string
-	cursor   int
-	selected map[int]struct{}
-}
 
-func get_conf_file() string {
-	file, err := os.Open("/home/hitori/.config/ttyfio/workflows.ttyfio")
+func get_conf_file() (string, string) {
 
-	file_data := make([]byte, 1024)
-	file.Read(file_data)
+	home_dir, err := os.UserHomeDir()
+
 	if err != nil {
-		return ""
+		fmt.Println("homedir could not be found", err)
+		home_dir = ""
 	}
-	return string(file_data)
+
+	
+	conf_file_path := fmt.Sprintf("%s/.config/ttyfio/workflows.ttyfio", home_dir)
+
+	file, err := os.Open(conf_file_path)
+	if err != nil {
+		fmt.Println("couldn't open conf file", err)
+	}
+	
+	file_data := make([]byte, 1024)
+
+	_, err = file.Read(file_data)
+	if err != nil {
+		return "", home_dir
+	}
+
+	return string(file_data), home_dir
 }
+
 func get_title() string {
-	return strings.Split(get_conf_file(), "<title>")[1]
+
+	data, _ := get_conf_file()
+	return strings.Split(data, "<title>")[1]
 }
 
-func read_from_workflows_file() []string {
-	data := get_conf_file()
-	workflows := strings.Fields(strings.Split(data, "<title>")[2])
-	return workflows[:len(workflows)-1]
+func read_parents() ([]string, string) {
+	data, home_dir := get_conf_file()
+	parsed := strings.Fields(strings.Split(data, "<title>")[2])[0]
+	parent_dirs := strings.Split(parsed, ":")
+	len_parent_dirs := len(parent_dirs)
+	return parent_dirs[:len_parent_dirs-1], home_dir
+}
 
+func read_children(parent_dir string) []string {
+	output, err := os.ReadDir(parent_dir)
+	var dirs []string
+	if err != nil {
+		fmt.Println("couldn't read children", err)
+	}
+
+	for idx := range output {
+		curr_dir := output[idx]
+
+		if curr_dir.Type().IsDir() {
+			dirs = append(dirs, curr_dir.Name())
+		}
+	}
+
+	return dirs
+}
+
+func all_dirs_consolidator() []choice {
+	var all_dirs []choice
+
+	parent_dirs, home_dir := read_parents()
+
+	counter := 0
+
+	for par_idx := range parent_dirs {
+		parent_dir := parent_dirs[par_idx]
+		constructed_dir := fmt.Sprintf("%s/%s/", home_dir, parent_dir)
+
+		children_dirs := read_children(constructed_dir)
+
+		for _, child := range children_dirs {
+			all_dirs = append(all_dirs, choice{parent_dir, child})
+			counter++
+		}
+	}
+
+	fmt.Println("this is alldirs", all_dirs)
+	fmt.Println("end alldirs")
+	return all_dirs
 }
 
 func initialModel() model {
+
 	return model{
-		choices:  read_from_workflows_file(),
+		choices:  all_dirs_consolidator(),
 		selected: make(map[int]struct{}),
 	}
 }
@@ -90,6 +166,7 @@ func (m model) View() string {
 	s := get_title()
 
 	s += "\n\n\n"
+
 	for i, choice := range m.choices {
 
 		cursor := " "
@@ -98,8 +175,6 @@ func (m model) View() string {
 		}
 
 		checked := " "
-		if _, ok := m.selected[i]; ok {
-		}
 
 		s += fmt.Sprintf("\n                                                 %s [%s] %s\n\n", cursor, checked, choice)
 
